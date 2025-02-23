a32136d30ae2473435e1f1ab8b3e8c94d5b92cae
ricky_lin <ricky_lin@trendmicro.com>
2025-02-23 16:04:12 -0600

    go...

--- amadeus.go
+++ amadeus.go
@@ -2,24 +2,22 @@ package amadeus
 
 import "errors"
 
-// Amadeus implements the FlightFeeProvider interface
 type Amadeus struct {
 	clientID     string
 	clientSecret string
 }
 
 func NewAmadeusProvider(clientID, clientSecret string) *Amadeus {
-	// 使用 clientID 和 clientSecret 初始化 Amadeus
 	return &Amadeus{clientID: clientID, clientSecret: clientSecret}
 }
 
 func (p *Amadeus) GetFlightFees(from, to string) (map[string]interface{}, error) {
-	// 实现获取费用的逻辑
+
 	if from == "" || to == "" {
 		return nil, errors.New("invalid input")
 	}
 	return map[string]interface{}{
 		"provider": "Amadeus",
-		"fee":      100.0, // 示例返回值
+		"fee":      100.0,
 	}, nil
 }
